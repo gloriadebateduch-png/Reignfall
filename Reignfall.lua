1a108d517c5b3ee7717bf91a6ac412f706a29974
gloriadebateduch-png <gloriadebateduch@gmail.com>
2026-09-19 15:10:44 +0700

    Update Rayfield library URL in Reignfall.lua

--- Reignfall.lua
+++ Reignfall.lua
@@ -3,7 +3,7 @@
 -- ========================================================================
 
 -- 1. LOAD LIBRARY RESMI RAYFIELD
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu'))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "🪦 Reign Fall: Husks Tracker",
